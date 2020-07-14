b85ec3fd8e889e6648d3ac174c3a0dfaa12b88c4
amrfatouh <66204477+amrfatouh@users.noreply.github.com>
2020-07-14 12:00:17 +0200

    assignment 2 - about us component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import '../App.css';
-import Menu from './MenuComponent'
 import Home from './HomeComponent'
+import Menu from './MenuComponent'
+import About from './AboutComponent'
 import Contact from './ContactComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
@@ -48,6 +49,7 @@ class Main extends Component {
                     <Route path="/home" component={this.HomePage} />
                     <Route exact path ="/menu" component={() => <Menu dishes={this.state.dishes} />} />
                     <Route path='/menu/:dishId' component={this.DishWithId} />
+                    <Route path='/aboutus' component={() => <About leaders={this.state.leaders} />}/>
                     <Route exact path='/contactus' component={Contact} />} />
                     <Redirect to="/home" />
                 </Switch>
