a71d6a55017a1c860bbce1c6105ff4b3d10acae3
amrfatouh <66204477+amrfatouh@users.noreply.github.com>
2020-07-14 13:01:28 +0200

    bug fixes - requesting wrong dish id

--- src/components/DishdetailComponent .js
+++ src/components/DishdetailComponent .js
@@ -17,7 +17,7 @@ function RenderDish({ dish }) {
 function RenderComments({ comments }) {
     const renderedComments = comments.map((eachComment) => {
         return (
-            <ul className="list-unstyled">
+            <ul className="list-unstyled" key={eachComment.id}>
                 <li>{eachComment.comment}</li>
                 <li>-- {eachComment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(eachComment.date)))}</li>
             </ul>
@@ -32,29 +32,38 @@ function RenderComments({ comments }) {
 }
 
 const DishDetail = ({ dish, comments }) => {
-    return (
-        <div className="container">
-            <div className="row">
-                <Breadcrumb>
-
-                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-                    <BreadcrumbItem active>{dish.name}</BreadcrumbItem>
-                </Breadcrumb>
-                <div className="col-12">
-                    <h3>{dish.name}</h3>
-                    <hr />
+    if(dish != null && comments != null) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+    
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{dish.name}</h3>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderDish dish={dish} />
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderComments comments={comments} />
+                    </div>
                 </div>
             </div>
-            <div className="row">
-                <div className="col-12 col-md-5 m-1">
-                    <RenderDish dish={dish} />
-                </div>
-                <div className="col-12 col-md-5 m-1">
-                    <RenderComments comments={comments} />
-                </div>
+        )
+    }
+    else {
+        return (
+            <div className="container">
+                <p>couldn't find the dish</p>
             </div>
-        </div>
-    )
+        );
+    }
 }
 
 export default DishDetail
